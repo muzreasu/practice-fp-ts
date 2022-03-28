1affd83c8c1242a574cd1f35cd8bbd37d8428da1
Xiaofeng Su <[xiaofeng.su@hyrecar.com](mailto:xiaofeng.su@hyrecar.com)>
2022-03-28 16:12:28 +0800

    add eslint rules

--- src/pipe-and-flow/practice_1.ts
+++ src/pipe-and-flow/practice_1.ts
@@ -2,10 +2,12 @@ const add1 = (num: number): number => num + 1;
 
 const multiply24 = (num: number): number => num * 24;
 
+// eslint-disable-next-line functional/no-return-void
 const addAndMultiplePipe = () => {
   // Implement given we have a number we need to add 1 to this number then multiple the result with 24 with the pipe functions.
 }
 
+// eslint-disable-next-line functional/no-return-void
 const addAndMultipleFlow = () => {
   // Implement given we have a number we need to add 1 to this number then multiple the result with 24 with the flow functions.
 }
