363b1045d72f715b85f29eed7d1c950a479dfc1c
Xiaofeng Su <[xiaofeng.su@hyrecar.com](mailto:xiaofeng.su@hyrecar.com)>
2022-03-28 16:12:12 +0800

    add practice description for pipe and flow

--- src/pipe-and-flow/pipe_practice_1.ts
+++
@@ -1,3 +0,0 @@
-const add1 = (num: number): number => num + 1;
-
-const multiply24 = (num: number): number => num * 24;
---
+++ src/pipe-and-flow/practice_1.ts
@@ -0,0 +1,11 @@
+const add1 = (num: number): number => num + 1;
+
+const multiply24 = (num: number): number => num * 24;
+
+const addAndMultiplePipe = () => {
+  // Implement given we have a number we need to add 1 to this number then multiple the result with 24 with the pipe functions.
+}
+
+const addAndMultipleFlow = () => {
+  // Implement given we have a number we need to add 1 to this number then multiple the result with 24 with the flow functions.
+}
