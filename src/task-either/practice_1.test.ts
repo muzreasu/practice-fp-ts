17d536a24f5a3808216a352b05a520b45a169839
xiaofengsu <xiaofeng.su@hyrecar.com>
2022-04-02 09:52:19 +0800

    add description for TE practice_1 test

--- src/task-either/practice_1.test.ts
+++ src/task-either/practice_1.test.ts
@@ -8,13 +8,13 @@ describe("reformatUUIDUsingTE", () => {
     jest.resetAllMocks();
   });
 
-  it("", async () => {
+  it("should return formatted UUID given get UUID succeed", async () => {
     (got.get as jest.Mock).mockResolvedValue({ body: "d484cea2-5fde-40e4-8169-3101c99bd292" });
 
     expect(await reformatUUIDUsingTE()).toEqual("d484cea2,5fde,40e4,8169,3101c99bd292");
   });
 
-  it("", async () => {
+  it("should return error given get UUID failed", async () => {
     (got.get as jest.Mock).mockRejectedValue("API error");
 
     expect(await reformatUUIDUsingTE()).toEqual("API error");
