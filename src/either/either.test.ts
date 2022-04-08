a67e06402d89a14ac9f31c58b418ee14136cf002
xiaofengsu <xiaofeng.su@hyrecar.com>
2022-04-08 15:56:54 +0800

    add more test cases to either practices

--- src/either/either.test.ts
+++ src/either/either.test.ts
@@ -1,10 +1,19 @@
 import { validateUUID } from "./either";
 
-describe('either', () => {
-  it('should return uuid given uuid is valid', () => {
-    expect(validateUUID('69f07739-a96e-4508-b5c7-7009baa4e9c5')).toEqual({"_tag": "Right", "right": ""})
-  })
-  it('should return invalid uuid error given uuid is invalid', () => {
-    expect(validateUUID('69f07739-a96e-4508-b5c7')).toEqual({"_tag": "Left", "left": "Invalid UUID"})
-  })
-})
+describe("either", () => {
+  it("should return uuid given uuid is valid", () => {
+    expect(validateUUID("69f07739,a96e4508,b5c7,7009baa4e9c5")).toEqual({ "_tag": "Right", "right": "" });
+  });
+
+  it("should return invalid uuid error given uuid is less than 36 characters", () => {
+    expect(validateUUID("69f07739-a96e-4508-b5c7")).toEqual({ "_tag": "Left", "left": "Invalid UUID" });
+  });
+
+  it("should return invalid uuid error given , is less than 4", () => {
+    expect(validateUUID("69f07739a96e4508,b5c7,7009baa4e9c5")).toEqual({ "_tag": "Left", "left": "Invalid UUID" });
+  });
+
+  it("should return invalid uuid error given letters is less than 4", () => {
+    expect(validateUUID("6960773929674508,b557,700945544955")).toEqual({ "_tag": "Left", "left": "Invalid UUID" });
+  });
+});
